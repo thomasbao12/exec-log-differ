34cf248f3fb22b57837f2e6acebcb05f9e053f44
Thomas Bao <thomas.bao@airbnb.com>
2021-06-16 14:42:39 -0700

    Get a dummy hello world binary

---
+++ BUILD
@@ -0,0 +1,4 @@
+java_binary(
+    name = "ExecLogParser",
+    srcs = glob(["src/main/java/com/airbnb/execlog_parser/*.java"])
+)
---
+++ src/main/java/com/airbnb/execlog_parser/ExecLogParser.java
@@ -0,0 +1,7 @@
+package com.airbnb.execlog_parser;
+
+public class ExecLogParser {
+  public static void main(String args[]) {
+    System.out.println("Hi!");
+  }
+}
